9bed787f70c2f0173a373738d59c21872b58b6b7
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 00:02:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// write code here
+function clone(arr){
+  return [...arr]
+}
+const originalArray = [1, 2, 3, 4, 5];
+const clonedArray = clone(originalArray);
+console.log(clonedArray); // [1, 2, 3, 4, 5]
+
